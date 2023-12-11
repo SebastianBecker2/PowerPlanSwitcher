31e0748085af1e516a96a2f7144c12c99a2c89bb
Sebastian Becker <mail@sbecker.de.com>
2023-12-11 22:06:11 +0100

    Add logging for unhandled exceptions

--- PowerPlanSwitcher/Program.cs
+++ PowerPlanSwitcher/Program.cs
@@ -1,9 +1,18 @@
 namespace PowerPlanSwitcher
 {
     using Properties;
+    using Serilog;
 
     internal static class Program
     {
+        private static readonly string AssemblyTitle =
+            AboutBox.AssemblyTitle ?? "";
+        private static readonly string LogFileName = $"{AssemblyTitle}.log";
+        private static readonly string LocalAppDataPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        private static readonly string LogPath =
+            Path.Combine(LocalAppDataPath, AssemblyTitle, LogFileName);
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -16,6 +25,16 @@ namespace PowerPlanSwitcher
                 Settings.Default.UpgradeRequired = false;
                 Settings.Default.Save();
             }
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+#pragma warning disable CA1305 // Specify IFormatProvider
+                using var log = new LoggerConfiguration()
+                    .WriteTo.File(LogPath)
+                    .CreateLogger();
+#pragma warning restore CA1305 // Specify IFormatProvider
+                log.Fatal(e.ExceptionObject as Exception, "Unhandled Exception");
+            };
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
