50788818d2be2f1eb75bd6ee35bdf57122db71aa
Sebastian Becker <mail@sbecker.de.com>
2024-08-04 18:54:11 +0200

    Move ActivePowerSchemeChangedEventArgs
    It belongs to the PowerManagement namespace.

--- PowerPlanSwitcherTests/PowerManagerStub.cs
+++ PowerPlanSwitcherTests/PowerManagerStub.cs
@@ -2,7 +2,6 @@ namespace PowerPlanSwitcherTests
 {
     using System;
     using System.Collections.Generic;
-    using PowerPlanSwitcher;
     using PowerPlanSwitcher.PowerManagement;
 
     internal class PowerManagerStub : IPowerManager
